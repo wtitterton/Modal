5ef55e29eafa232594de5cda6cf8de256bda0843
william titterton <wftitterton@gmail.com>
2018-06-23 15:21:27 +0100

    added animations

--- js/Modal.js
+++ js/Modal.js
@@ -9,7 +9,10 @@
             overlay: $("<div class='overlay'></div>"),
             container:$("<div class='modal-container'></div>"),
             openBtn:'.test',
-            overlayColor:"rgba(0,0,0,.3)"
+            overlayColor:"rgba(0,0,0,.3)",
+            HTMLContent: "",
+            closeBtn:$("<i class='fas fa-times'></i>"),
+            animationClass: "slide-down"
         }
         var modal = this;
         modal.settings = {}
@@ -18,8 +21,12 @@
             modal.settings = $.extend({}, defaults, options);
             modal.el = el;
             $(modal.el).append(modal.settings.overlay);
+            $(modal.settings.overlay).addClass(modal.settings.animationClass);
             $(modal.settings.overlay).append(modal.settings.container);
+            $(modal.settings.container).append(modal.settings.closeBtn);
+            $(modal.settings.container).append(modal.settings.HTMLContent);
             $(modal.settings.openBtn).on('click', modal.open);
+            $(modal.settings.closeBtn).on('click',modal.close);
 
             // apply style options
             $(modal.settings.overlay).css({
@@ -30,12 +37,12 @@
         // public methods
         modal.open = function()
         {
-          $(modal.settings.overlay).addClass('show');
+          $(modal.settings.overlay).addClass('open');
           $(modal.el).addClass('no-scroll');
         }
         modal.close = function()
         {
-          $(modal.settings.overlay).removeClass('show');
+          $(modal.settings.overlay).removeClass('open');
           $(modal.el).removeClass('no-scroll');
         }
         // private methods
--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
+
+  var html = "<div class='content'>";
+  html += "<h1>Modal heading</h1>";
+  html += "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </p>";
+  html += "<h6 class='button'>Click Here</h6>"
+  html += "</div>";
+
   var myplugin = new $.Modal($('body'),{
-    openBtn: ".overlayBtn"
+    openBtn: ".overlayBtn",
+    HTMLContent: html,
+    animationClass: "simple"
   });
 });
